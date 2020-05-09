94f2ec01a1b801330fa7445a36ccbbab1c7ab97b
Lai Khang Duy <laiduy98@gmail.com>
2020-05-09 11:32:48 +0700

    polybar

--- .config/i3/audio.sh
+++ .config/i3/audio.sh
@@ -6,7 +6,11 @@ INDEXINUSE=$(pacmd list-sinks | grep "index:" | grep '*' | awk '{print $3}')
 if [[ ! "${1}" =~ ^[-+] ]]; then
     pactl set-sink-mute $INDEXINUSE toggle
 else
-    current=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
+    if [ $INDEXINUSE -ne 0 ]; then
+        current=$(pacmd dump-volumes | awk 'NR==2{print $8}' | sed 's/\%//')
+    else
+        current=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
+    fi
     if [ $current -le 100 ]; then
         pactl set-sink-volume $INDEXINUSE $1
     else
