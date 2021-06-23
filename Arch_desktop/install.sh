6f6d8ae1a7e88a8633532340485f461c39cfdf57
Lai Khang Duy <laiduy98@gmail.com>
2021-06-23 23:31:30 +0700

    add dunst and change color xfce

--- Arch_desktop/install.sh
+++ Arch_desktop/install.sh
@@ -1,6 +1,7 @@
 sudo pacman -Syu
-sudo pacman -S neofetch neovim htop picom i3-gaps zsh git imwheel rofi feh ttf-freefont ttf-arphic-uming ttf-baekmuk neovim lxappearance lightdm-gtk-greeter-settings ibus ntp man noto-fonts-emoji gnome-keyring xarchiver qalculate-gtk
-yay -S polybar rambox google-chrome visual-studio-code-bin discord ibus-bamboo arc-gtk-theme pop-icon-theme skype pycharm datagrip
+sudo pacman -S neofetch neovim htop picom i3-gaps zsh git imwheel rofi feh ttf-freefont ttf-arphic-uming ttf-baekmuk neovim lxappearance lightdm-gtk-greeter-settings ibus ntp man noto-fonts-emoji gnome-keyring xarchiver qalculate-gtk dunst
+curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | gpg --import -
+yay -S polybar rambox google-chrome visual-studio-code-bin discord ibus-bamboo arc-gtk-theme pop-icon-theme skype pycharm datagrip spotify
 systemctl enable ntpd
 timedatectl set-ntp 1
 sudo hwclock --systohc
